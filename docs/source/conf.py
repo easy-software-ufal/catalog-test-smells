4959435657d57943562b62b58d59d0d1f107ad16
Davi da Silva Romão <53953664+daviromao@users.noreply.github.com>
2023-05-09 11:41:36 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 
 project = 'Test Smells Catalog'
 copyright = '2023, EASY'
-author = 'Engineering and Systems Software Research Group - Universidade Federal de Alagoas (UFAL)'
+author = 'Engineering and Systems Software Research Group\n - Universidade Federal de Alagoas (UFAL)'
 
 release = '2.0'
 version = '2.3.0'
